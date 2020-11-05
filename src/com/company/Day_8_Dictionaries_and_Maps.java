d1aa6509ca50b7eb63e3a776b3f95ef6c97aba51
Thilaka Vijayan <v.thilaka@gmail.com>
2020-11-05 13:16:30 +1100

    Day 8

--- src/com/company/Day_8_Dictionaries_and_Maps.java
+++ src/com/company/Day_8_Dictionaries_and_Maps.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
  */
 public class Day_8_Dictionaries_and_Maps{
 
-    public static void main(String []argh){
+    public static void main(String[] args){
         Scanner in = new Scanner(System.in);
 
         Map<String, Integer> phoneBook = new HashMap<>();
